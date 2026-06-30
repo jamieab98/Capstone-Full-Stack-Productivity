943b0d7f8fc824eaae73f0350b044eed683b2578
Jamie Bryson <jamieab98@gmail.com>
2026-06-30 18:32:23 -0400

    created the handleCreateUser function and the form to go with it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react"
 
 function App(){
   const [newUsername, setNewUsername] = useState("")
+  const [displayName, setDisplayName] = useState("")
+  const [password, setPassword] = useState("")
 
   function handleCreateUser(e){
     e.preventDefault()
     console.log(newUsername)
+    console.log(displayName)
+    console.log(password)
   }
 
   return(
@@ -16,13 +20,13 @@ function App(){
         <h4>Create User</h4>
         <form onSubmit={handleCreateUser}>
           <label htmlFor="username">User Name</label>
-          <input type="text" value={newUsername} id="username"onChange={(e)=>setNewUsername(e.target.value)}/>
+          <input type="text" value={newUsername} id="username" onChange={(e)=>setNewUsername(e.target.value)}/>
 
           <label htmlFor="displayname">Display Name</label>
-          <input type="text" id="displayname"/>
+          <input type="text" value={displayName} id="displayname" onChange={(e)=>setDisplayName(e.target.value)}/>
 
           <label htmlFor="password">Password</label>
-          <input type="text" id="password"/>
+          <input type="password" value={password} id="password" onChange={(e)=>setPassword(e.target.value)}/>
 
           <button type="submit">Create User</button>
         </form>
