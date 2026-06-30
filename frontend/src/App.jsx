28596b7cbcbcc158c3b6b08af46d1bcfa7c42e33
Jamie Bryson <jamieab98@gmail.com>
2026-06-30 18:51:24 -0400

    created the handleCreateWorkout function and Create Workout form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,23 @@ function App(){
   const [newUsername, setNewUsername] = useState("")
   const [displayName, setDisplayName] = useState("")
   const [password, setPassword] = useState("")
+  
+  const [workoutType, setWorkoutType] = useState("")
+  const [workoutDate, setWorkoutDate] = useState("")
+  const [username, setUsername] = useState("")
 
   function handleCreateUser(e){
     e.preventDefault()
     console.log(newUsername)
     console.log(displayName)
     console.log(password)
+  }
+
+  function handleCreateWorkout(e){
+    e.preventDefault()
+    console.log(workoutType)
+    console.log(workoutDate)
+    console.log(username)
   }
 
   return(
@@ -31,6 +42,28 @@ function App(){
           <button type="submit">Create User</button>
         </form>
       </div>
+
+      <h4>Create Workout</h4>
+      <form onSubmit={handleCreateWorkout}>
+        <label htmlFor="workouttype">Workout Type</label>
+        <select value={workoutType} onChange={(e)=>setWorkoutType(e.target.value)} id="workouttype">
+          <option value="leg">Leg</option>
+          <option value="push">Push</option>
+          <option value="pull">Pull</option>
+          <option value="upper">Upper</option>
+          <option value="lower">Lower</option>
+          <option value="full">Full Body</option>
+          <option value="other">Other</option>
+        </select>
+
+        <label htmlFor="date">Date: </label>
+        <input type="date" id="date" value={workoutDate} onChange={(e)=>setWorkoutDate(e.target.value)}></input>
+
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" value={username} onChange={(e)=>setUsername(e.target.value)}></input>
+
+        <button type="submit">Create Workout</button>
+      </form>
       <div>
         <h4>Information Display</h4>
       </div>
