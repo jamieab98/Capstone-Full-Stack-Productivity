78881d8887fd96100b7e718a79d822c07633bc2e
Jamie Bryson <jamieab98@gmail.com>
2026-06-30 07:16:45 -0400

    started setting up a mimimalistic frontend to create and test post patch and delete methods

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react"
+
 function App(){
+  const [newUsername, setNewUsername] = useState("")
+
+  function handleCreateUser(e){
+    e.preventDefault()
+    console.log(newUsername)
+  }
 
   return(
     <>
       <h1>My App</h1>
+      <div>
+        <h3>Mock Up Form</h3>
+        <h4>Create User</h4>
+        <form onSubmit={handleCreateUser}>
+          <label htmlFor="username">User Name</label>
+          <input type="text" value={newUsername} id="username"onChange={(e)=>setNewUsername(e.target.value)}/>
+
+          <label htmlFor="displayname">Display Name</label>
+          <input type="text" id="displayname"/>
+
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password"/>
+
+          <button type="submit">Create User</button>
+        </form>
+      </div>
+      <div>
+        <h4>Information Display</h4>
+      </div>
     </>
   )
 }
