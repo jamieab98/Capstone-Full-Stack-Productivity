32f4c6c442d2678f049deadbfb63b85e40d7e280
Jamie Bryson <jamieab98@gmail.com>
2026-07-02 06:54:10 -0400

    updated the models to fully match to front end

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,10 @@ from extensions import db
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    workouts = db.relationship("Workout", backref="user")
+    username = db.Column(db.String, unique=True)
+    display_name = db.Column(db.String)
+    password = db.Column(db.String)
+    workouts = db.relationship("Workout", backref="user", cascade="all, delete-orphan")
 
 class Workout(db.Model):
     __tablename__ = "workouts"
@@ -12,14 +14,14 @@ class Workout(db.Model):
     workout_type = db.Column(db.String)
     date = db.Column(db.Date)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    exercises = db.relationship("Exercise", backref="workout")
+    exercises = db.relationship("Exercise", backref="workout", cascade="all, delete-orphan")
 
 class Exercise(db.Model):
     __tablename__ = "exercises"
     id = db.Column(db.Integer, primary_key=True)
     exercise_name = db.Column(db.String)
     workout_id = db.Column(db.Integer, db.ForeignKey("workouts.id"))
-    exercise_sets = db.relationship("ExerciseSet", backref="exercise")
+    exercise_sets = db.relationship("ExerciseSet", backref="exercise", cascade="all, delete-orphan")
 
 class ExerciseSet(db.Model):
     __tablename__ = "exercise_sets"
